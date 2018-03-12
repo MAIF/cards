55aab446ab06fa435a3b375f2f51307c793ce70c
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-03-12 20:43:02 +0100

    Add all needed links

--- generator.js
+++ generator.js
@@ -56,7 +56,9 @@ function createIndex(category, card) {
 		${allCards.map(card => createCardFragment(card)).join('\n')}
 	</div>
 	<div id="random"></div>
+	<div style="width:100%;display:flex;justify-content:center;align-items:center;">
 	<button id="random-click" type="button" class="btn btn-success">random card</button>
+	</div>
 	<script>
 		$(function() {
 			function onclickrandom() {
