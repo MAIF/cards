3d984b22987eb6e1e8805e54b6201f2320297526
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-03-20 17:23:22 +0100

    Add final url

--- generator.js
+++ generator.js
@@ -284,7 +284,7 @@ function basePage(title, content, search = true, reload = false) {
 			<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
 			<title>${title}</title>
       <meta name="description" content="${title}" />
-      <meta property="og:url" content="URL FINALE" />
+      <meta property="og:url" content="https://maif.github.io/cards/" />
       <meta property="og:type" content="article" />
       <meta property="og:title" content="${title}" />
       <meta property="og:description" content="${title}" />
