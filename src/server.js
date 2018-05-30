6f7ba0dc78843fc3a537b8d9397834543ead7e28
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-05-30 09:08:09 +0200

    Move stuff around

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/cards/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../docs' + req.path.replace('/cards/', '/')))
+})
+
+app.listen(3000, () => console.log('Server listening on port 3000!'))
