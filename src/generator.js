59612664d74afcd93c02ed2da1db15038587d8f1
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-05-30 13:34:11 +0200

    Fix links

--- src/generator.js
+++ src/generator.js
@@ -157,7 +157,7 @@ function createCardFragment(language, card, rotate = false) {
 	if (card.lang[lang].abstract.length === 0 && card.lang[lang].details.length === 0) {
     // tete de categorie
 		return `
-		<a class="any-card" href="/cards/${card.category}/index.html">
+		<a class="any-card" href="/cards/${lang}/${card.category}/index.html">
 			<div class="row categ-card">
 				<div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-5 col-md-offset-1 col-lg-4  col-lg-offset-2 categ-left">
 					<div class="covercard covercard-${card.category}">
@@ -176,7 +176,7 @@ function createCardFragment(language, card, rotate = false) {
 	const abstract = converter.makeHtml(card.lang[lang].abstract.join('\n\n'));
 	const details = converter.makeHtml(card.lang[lang].details.join('\n\n'));
 	return `
-	<a class="any-card complete-card container-fluid" href="/cards/${card.category}/${card.id}.html" data-content="${(card.lang[lang].title + ' - ' + card.lang[lang].abstract + ' - ' + card.lang[lang].details).toLowerCase()}">
+	<a class="any-card complete-card container-fluid" href="/cards/${lang}/${card.category}/${card.id}.html" data-content="${(card.lang[lang].title + ' - ' + card.lang[lang].abstract + ' - ' + card.lang[lang].details).toLowerCase()}">
 		<div class="row card">
 			<div class="col-xs-12 col-sm-5 col-md-5 col-md-offset-1 col-sm-offset-1 col-lg-4 col-lg-offset-2 ${rotate ? 'left' : 'categ-left'}">
 				<div class="cardfront cardfront-${card.category}-${card.golden ? 'golden' : 'normal'}">
